6fdddd3248ee61e0212d61c6cb5004fb9ecf6760
MariosKosmas <kosma@outlook.com.gr>
2022-01-04 14:26:24 +0200

    reply to ticket

--- src/components/breadcrumb/Breadcrumb.comp.js
+++ src/components/breadcrumb/Breadcrumb.comp.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { Breadcrumb } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import {Breadcrumb} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
 
-export const PageBreadcrumb = ({ parent, page }) => {
-  return (
-    <Breadcrumb>
-      <LinkContainer to="/">
-        <Breadcrumb.Item>{parent}</Breadcrumb.Item>
-      </LinkContainer>
-      <Breadcrumb.Item active>{page}</Breadcrumb.Item>
-    </Breadcrumb>
-  );
+export const PageBreadcrumb = ({parent = 'Home', parentLink = "/", page}) => {
+    return (
+        <Breadcrumb>
+            <LinkContainer to={parentLink}>
+                <Breadcrumb.Item>{parent}</Breadcrumb.Item>
+            </LinkContainer>
+            <Breadcrumb.Item active>{page}</Breadcrumb.Item>
+        </Breadcrumb>
+    );
 };
--- src/modules/ticket/pages/details/details.js
+++ src/modules/ticket/pages/details/details.js
@@ -1,6 +1,5 @@
 import React, {useEffect} from "react";
-import {Alert, Col, Row, Spinner} from "react-bootstrap";
-import {PageBreadcrumb} from "../../../../components/breadcrumb/Breadcrumb.comp";
+import {Col, Row, Spinner} from "react-bootstrap";
 
 import {useParams} from "react-router";
 import useTicketAction from "../../hooks/useTicketAction";
@@ -12,24 +11,6 @@ export default () => {
     const {id} = useParams();
     const {getTicketDetails} = useTicketAction()
     const {byId: ticket, loading} = useTicketData({id})
-    const
-        replyMsg = null,
-        replyTicketError = null
-
-    // const {
-    //     isLoading,
-    //     error,
-    //     selectedTicket,
-    //     replyMsg,
-    //     replyTicketError,
-    // } = useSelector(state => state.tickets);
-
-    // useEffect(() => {
-    // 	dispatch(fetchSingleTicket(tId));
-    // 	return () => {
-    // 		(replyMsg || replyTicketError) && dispatch(resetResponseMsg());
-    // 	};
-    // }, [tId, dispatch, replyMsg, replyTicketError]);
 
     useEffect(() => {
         getTicketDetails(id)
@@ -41,9 +22,6 @@ export default () => {
 
     return (
         <DefaultLayout>
-            {/*<h1 className="pb-2 mt-4 mb-2 border-bottom">*/}
-            {/*    Ticket Details*/}
-            {/*</h1>*/}
             <Row>
                 <Col>
                 </Col>
@@ -51,11 +29,6 @@ export default () => {
             <Row>
                 <Col>
                     {loading && <Spinner variant="primary" animation="border"/>}
-                    {/*{error && <Alert variant="danger">{error}</Alert>}*/}
-                    {replyTicketError && (
-                        <Alert variant="danger">{replyTicketError}</Alert>
-                    )}
-                    {replyMsg && <Alert variant="success">{replyMsg}</Alert>}
                 </Col>
             </Row>
             {ticket && (
